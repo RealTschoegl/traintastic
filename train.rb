2166598dde2171b2f3ae1678cf8ca7de5d0ce99d
MakerSquare Student <students@themakersquare.com>
2013-07-23 11:53:55 -0500

    Added comment to duration method

--- train.rb
+++ train.rb
@@ -17,6 +17,13 @@ class Train
     end
   end
 
+  # Public: Calculate the distance of a trip based on its duration
+  #
+  # duration - The integer value that represents the length of the train trip, in hours
+  #
+  # Returns the trip distance in miles
+
+
   def trip_distance(duration)
     duration * @speed
   end
